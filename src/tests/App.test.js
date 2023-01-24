926074cf2523aa3b00f4d69b1a74992223b3b5b9
Karina Gonzalez <karinagonzalez99@gmail.com>
2023-01-24 14:26:45 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando componente App.js', () => {
+  it('link Home', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: /Home/i });
+    expect(home).toBeDefined();
+  });
+
+  it('link About', () => {
+    renderWithRouter(<App />);
+    const about = screen.getByRole('link', { name: /About/i });
+    expect(about).toBeDefined();
+  });
+
+  it('link Favorite Pokémon', () => {
+    renderWithRouter(<App />);
+    const favoritePokemon = screen.getByRole('link', { name: /Favorite Pokémon/i });
+    expect(favoritePokemon).toBeDefined();
+  });
+});
